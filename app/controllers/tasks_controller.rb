bb793005ddb1e9fcc73e12ec793e58f0921ddfd1
shogo.sato <ss.rk1993@gmail.com>
2019-11-30 14:35:33 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
-    @tasks=Task.all
+    @tasks=current_user.tasks.order(id: :desc)
   end
 
   def show
@@ -8,11 +10,13 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task=Task.new
+    #@task=Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task=Task.new(task_params)
+    #@task=Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
       if @task.save
        flash[:success]="タスクが正常に投稿されました"
